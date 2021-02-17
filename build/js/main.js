2158e4e9f795e25f8bbde14192da98aa47034001
alex-suschin <alex_suschin@mail.ru>
2021-02-17 16:47:42 +0300

    main-desk

--- build/js/main.js
+++ build/js/main.js
@@ -10,6 +10,32 @@ $(function() {
         }
     });
 
+    ymaps.ready(function() {
+        var myMap = new ymaps.Map('map', {
+                center: [51.709612, 39.152347],
+                zoom: 17,
+                scrollZoom: false,
+                controls: ['zoomControl']
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+
+            myPlacemark = new ymaps.Placemark([51.709773, 39.154419], {
+                hintContent: 'ООО ГК "АГРО-ЛИДЕР"',
+                balloonContent: 'ООО ГК "АГРО-ЛИДЕР"'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: '../img/gps.svg',
+                iconImageSize: [47, 68],
+                cursor: 'pointer',
+                iconImageOffset: [-20, -70],
+                balloonclose: true
+            });
+        myMap.behaviors.disable('scrollZoom');
+        myMap.geoObjects
+            .add(myPlacemark)
+
+    });
 
 });
 
